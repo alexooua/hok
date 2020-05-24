d2e311c5e26007425f2959db05120352fc2a9681
alex <alextotalk@gmail.com>
2020-05-24 23:49:36 +0300

    First

---
+++ src/Example.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+function Example() {
+    // Объявляем новую переменную состояния "count"
+    const [count, setCount] = useState(0);
+    const [age, setAge] = useState(42);
+    const [fruit, setFruit] = useState('банан');
+    const [todos, setTodos] = useState([{ text: 'Изучить хуки' }]);
+    return (
+        <div>
+            <p>Вы нажали {count} раз</p>
+            <p>{age}{fruit}{todos}</p>
+            <button onClick={() => setCount(count + 1)}>
+                Нажми на меня
+            </button>
+        </div>
+    );
+}
+export default Example
